17550daa156c68e48026a7acd71f3176e95762c6
Salman Shtayeh <sshtayeh@itgsoftware.com>
2026-04-06 01:11:03 +0300

    category component

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -10,6 +10,11 @@ const Category = () => {
             <h2>sub category</h2>
             <h3>sub sub category</h3>
             <h4>sub sub sub category</h4>
+
+            <h1>Category</h1>
+            <h2>sub category</h2>
+            <h3>sub sub category</h3>
+            <h4>sub sub sub category</h4>
         </div>
     );
 };
