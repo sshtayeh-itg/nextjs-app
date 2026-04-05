55df205e88b286d60db07379b16e453c2c23e61b
Salman Shtayeh <sshtayeh@itgsoftware.com>
2026-04-05 16:49:58 +0300

    update header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+
 const Header = () => {
+    const [data, setData] = useState([]); // store JSON array
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const str = "Hello World";
+  }, []);
+
+  useEffect(() => {
+    // fetch from dummy API
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);     // store data in state
+        setLoading(false); // stop loading
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+
+    //   console.log(data);
+    console.log("data ---", data);
+  }, []); // empty dependency array → runs once on mount
+
+  if (loading) return <p>Loading...</p>;
+
+
   return (
     <header>
       <h1>Header</h1>
+
     </header>
   );
 };
