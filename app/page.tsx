17331e33479fe5a9635dcb49fba224e195ca6d5a
Salman Shtayeh <sshtayeh@itgsoftware.com>
2026-04-06 17:27:57 +0300

    update  page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1> NextJs App </h1>
+      <h1> NextJs App </h1>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
