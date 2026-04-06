d5059a4424b0ca99fc411423f3630dedd392fd26
Salman Shtayeh <sshtayeh@itgsoftware.com>
2026-04-06 18:44:59 +0300

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+  debugger;
+  console.log("NextJs App");
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1> NextJs App </h1>
+      <h1> NextJs App </h1>
       <h1> NextJs App </h1>
       <h1> NextJs App </h1>
       <h1> NextJs App </h1>
